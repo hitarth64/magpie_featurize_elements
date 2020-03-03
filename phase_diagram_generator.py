02ddd522594e5698078e0d425e6304175c4962c4
Hitarth Choubisa <hitarthc64@gmail.com>
2020-03-02 22:07:28 -0500

    Create phase_diagram_generator.py

---
+++ phase_diagram_generator.py
@@ -0,0 +1,18 @@
+from pymatgen import MPRester
+from pymatgen.entries.compatibility import MaterialsProjectCompatibility
+from pymatgen.analysis.phase_diagram import *
+from pymatgen.io.ase import *
+from pymatgen.io.vasp import Vasprun
+
+MAPI_KEY = 'your_mapi_key'
+mpr = MPRester(MAPI_KEY)
+
+compat = MaterialsProjectCompatibility()
+
+system = ["C","H","N","Cl"]
+unprocessed_entries = mpr.get_entries_in_chemsys(system)
+processed_entries = compat.process_entries(unprocessed_entries)
+pd = PhaseDiagram(processed_entries)
+vrun = Vasprun('vasprun.xml')
+struct = AseAtomsAdaptor.get_structure(read('POSCAR'))
+obj = PDEntry(struct.composition, energy=vrun.final_energy)
